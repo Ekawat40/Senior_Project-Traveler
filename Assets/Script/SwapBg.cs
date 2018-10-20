7eca86e401f78f3e06cf31a159afdefffb396296
Ekawat40 <aekwatt@hotmail.com>
2018-10-20 11:26:16 +0700

    push project

---
+++ Assets/Script/SwapBg.cs
@@ -0,0 +1,122 @@
+﻿namespace PedometerU.Tests
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using UnityEngine.SceneManagement;
+
+    public class SwapBg : MonoBehaviour
+    {
+        Scene sceneM;
+        public SpriteRenderer part;
+        public Sprite[] options;
+        public int index;
+        public Text stepText, distanceText,testText;
+        private Pedometer pedometer;
+        private int num, numBg;
+
+
+        private void Start()
+        {
+            sceneM = SceneManager.GetActiveScene();
+            // Create a new pedometer
+            pedometer = new Pedometer(OnStep);
+            // Reset UI
+            OnStep(0, 0);
+
+            
+        }
+
+        private void OnStep(int steps, double distance)
+        {
+            // Display the values // Distance in feet
+            num = steps;
+            numBg = steps;
+            testText.text = numBg.ToString();
+            stepText.text = num.ToString();
+            //stepText.text = steps.ToString();
+            //distanceText.text = (distance * 3.28084).ToString("F2") + " ft";
+        }
+
+        private void OnDisable()
+        {
+            // Release the pedometer
+            pedometer.Dispose();
+            pedometer = null;
+        }
+
+        void Update()
+        {
+            for (int i = 0; i < options.Length; i++)
+            {
+                if (i == index)
+                {
+                    part.sprite = options[i];
+                }
+            }
+
+            Cbg();
+        }
+
+        public void Swap()
+        {
+            if (index < options.Length - 1)
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+        }
+
+        public void Cbg()
+        {
+            /*if (num>15)
+            {
+                index++;
+                num = 0;
+            }
+            else 
+            {
+                index = 0;
+            }*/
+
+
+            // ถ้านับก้าวครบทุกๆ15ก้าวจะเปลี่ยนประเทศ 
+
+            if (numBg == 40)
+            {
+                index++;
+            }
+            else if(numBg ==80)
+            {
+                index = 0;
+            }
+        }
+
+        public void Addnum()
+        {
+            num += 10;
+        }
+
+
+        public void btnCity()
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+
+        public void btnShop()
+        {
+            SceneManager.LoadScene("ShopItem");
+        }
+
+        public void btnmain()
+        {
+            SceneManager.LoadScene("Main_1");
+        }
+
+
+    }
+}
---
+++ Assets/ShopTutorialAssets/ShopControlScript.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ShopControlScript : MonoBehaviour {
+
+
+    public SpriteRenderer part;
+    public Sprite[] options;
+    public int index;
+
+
+    int moneyAmount;
+	int isLootboxSold;
+
+	public Text moneyAmountText;
+	public Text LootboxPrice;
+
+	public Button buyButton;
+
+	// Use this for initialization
+	void Start () {
+		moneyAmount = 200;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        for (int i = 0; i < options.Length; i++)
+        {
+            if (i == index)
+            {
+                part.sprite = options[i];
+            }
+        }
+
+
+	}
+
+	public void buyLootbox()
+	{
+
+
+
+       moneyAmount -= 200;
+        PlayerPrefs.SetInt("IsLootboxSold", 1);
+        LootboxPrice.text = "Sold!";
+        buyButton.gameObject.SetActive(false);
+    }
+
+
+    public void Swap()
+    {
+
+        int randomNumber = Random.Range(1, 4);
+
+        /*if (index < options.Length - 1)
+        {
+            index++;
+        }
+        else
+        {
+            index = 0;
+        }*/
+        if(randomNumber == 1)
+        {
+            index = 1;
+        }else if(randomNumber == 2)
+        {
+            index = 2;
+        }
+        else if (randomNumber == 3)
+        {
+            index = 3;
+        }
+
+    }
+
+
+        public void btnCity()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void btnShop()
+    {
+        SceneManager.LoadScene("ShopItem");
+    }
+
+    public void btnmain()
+    {
+        SceneManager.LoadScene("Main_1");
+    }
+
+
+
+}
